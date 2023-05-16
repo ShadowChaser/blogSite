b260b66b1ecd2b6ca671b7d30e04aafcd6c613e1
Abhishek <abhishekkundu86@gmail.comm>
2023-05-16 12:10:06 +0530

    Added Single Page

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png'
+import { Link } from 'react-router-dom';
 
 export default function Single() {
   return (
-    <div>
-      Single
+    <div className='single'>
+      <div className='content'>
+          <img src="https://images.pexels.com/photos/1144265/pexels-photo-1144265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+          <div className='user'>
+            <img src="https://images.pexels.com/photos/6510487/pexels-photo-6510487.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt=""/>
+          <div className='info'> 
+              <span>John</span>
+              <p>Posted 2 days ago</p>
+          </div>
+          <div className='edit'>
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt=""/>
+            </Link>
+            
+            <img src={Delete} alt=""/>
+          </div>
+          </div>
+          <h1> Lorem</h1>
+          <p> loredasasdashd asbdasuid hasbdiuasdhbasuasid basud asudbas ui dbn uionas dioasdasdas
+            asdasdasdhasiudashd asi dasj doiasdasdioasjdoas das\d asd\\d 
+            dasdjash dasui dashdasiodhjasodiasj  odiasdas
+            d asdhasdbas dasnoi dasiodioasmd
+
+            dasdasidhasiod asuiodasudoas djasodjasdasiu j dasjiop  djasdopa
+            sdahjdbas judasbdaskidkbnasdkasd
+
+            dashjdasgdiashdioas  hj daspo dhas opdasopdasudjasopdas
+
+
+            dasdjasdghasiud asiods
+          </p>
+      </div>
+      <div className='menu'>m</div>
     </div>
   )
 }
